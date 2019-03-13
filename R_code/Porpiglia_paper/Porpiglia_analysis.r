0ed17e64a369b18d388733ca661d39beb1ae000f
Darren Tyson (office) <darren.tyson@vanderbilt.edu>
2019-03-13 11:52:37 -0500

    Updated code for Porpiglia data extraction from FCS files in preparation of dimensionality reduction, clustering, etc.

--- R_code/Porpiglia_paper/Porpiglia_analysis.r
+++ R_code/Porpiglia_paper/Porpiglia_analysis.r
@@ -6,24 +6,50 @@ library(openCyto)
 library(MEM)
 library(tidyverse)
 
-
-# a <- lapply(pf.FCS[[1]], function(x)  as.data.frame(x@exprs))
-# n <- basename(names(a))
-# 
-# for(i in n) a[[grep(i,names(a))]] <-  
-#     cbind( a[[grep(i,names(a))]],
-#            data.frame(sample=rep(i,nrow(a[[grep(i,names(a))]]))))
-# 
-# a <- do.call(rbind,a)
-# rownames(a) <- NULL
-# 
-# s <- a[grep('weeks_1',a$sample),][1:100000,]
-# 
-# #xyplot(`DNA1(Ir191)Dd` ~ `Cell_length` | `sample`, data=a, pch='.')
-# 
-# markers <- lapply(pf.FCS, function(x)  unique(unlist(sapply(x, function(z) z@parameters@data['desc']))))
-
+# get file names of FCS files
 f <- list.files('ZY3K_filtered_files',full.names=TRUE)
+
+# read files into list
 d <-  lapply(f, read.FCS)
 
-n <- unique(unlist(lapply(d, colnames)))
+# colnames are unique metals detected in different Cytof channels
+channels <- unique(unlist(lapply(d, colnames)))
+# extract marker names from descriptions
+markers <- unique(unlist(sapply(d, function(z) z@parameters@data['desc'])))
+
+# keep only expression level data
+d <- lapply(d, function(x) as.data.frame(x@exprs))
+
+# extract sample name and replicate number from file names
+# first remove extraneous
+samp_names <- gsub('EP20160824-InjuryTimeCourse-','',basename(f))
+samp_names <- gsub('_alpha7__CD9_.fcs','',samp_names)
+# extract replicate number from samp_names
+rep_num <- sapply(samp_names, function(x) strsplit(x,'_')[[1]][2])
+# remove replicate number from samp_names
+samp_names <- sapply(samp_names, function(x) strsplit(x,'_')[[1]][1])
+
+
+# add columns for sample name and replicate
+d <- lapply(seq_along(d), 
+    function(x) cbind(
+        d[[x]],
+        data.frame( sample=rep(samp_names[x],nrow(d[[x]])),
+                    rep=rep(rep_num[x],nrow(d[[x]])))))
+
+# use same number of cells from each sample and replicate
+# lowest number of cells in any sample is 1854
+# set a seed if you want this to always produce the same result
+set.seed(1854)
+d <- lapply(d, function(x) sample_n(x,1854))
+
+# combine all data into single data.frame (tibble)
+d <- do.call(rbind, d)
+colnames(d) <- c(markers,'sample','rep')
+
+# remove event length and beadDist columns
+d <- d[,!colnames(d) %in% c('beadDist','Event_length')]
+# put sample and rep columns first 
+d <- d[,c('sample','rep',head(colnames(d),ncol(d)-2))]
+
+### Data should now be ready for applying dimensionality reduction, clustering, etc
